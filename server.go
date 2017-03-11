14da549324e33bd76547019c53eef713805951f4
211211 <nhquan307@outlook.com>
2017-03-12 01:05:51 +0700

    login with session. Decode session and display username

--- server.go
+++ server.go
@@ -6,7 +6,39 @@ import (
 	"fmt"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/securecookie"
 )
+
+// cookie handling
+
+var cookieHandler = securecookie.New(
+	securecookie.GenerateRandomKey(64), // hash key at least 32 bytes
+	securecookie.GenerateRandomKey(32)) // block key at least 16 bytes
+
+func getUsername(request *http.Request) string {
+	if cookie, err := request.Cookie("session"); err == nil {
+		cookieValue := make(map[string]string)
+		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+			return cookieValue["username"]
+		}
+	}
+	return ""
+}
+
+func createSession(username string, response http.ResponseWriter) {
+	value := map[string]string{
+		"username": username,
+	}
+
+	if encoded, err := cookieHandler.Encode("session", value); err == nil {
+		cookie := &http.Cookie{
+			Name:  "session",
+			Value: encoded,
+			Path:  "/",
+		}
+		http.SetCookie(response, cookie)
+	}
+}
 
 // index page
 
@@ -35,6 +67,8 @@ func loginHandler(response http.ResponseWriter, request *http.Request) {
 	redirectURL := "/"
 
 	if username != "" && password != "" {
+		// TODO check username and password
+		createSession(username, response)
 		redirectURL = "/profile"
 	}
 
@@ -51,13 +85,20 @@ func logoutHandler(response http.ResponseWriter, request *http.Request) {
 
 const profilePage = `
 <h1>Profile</h1>
+<small>Username: %s</small>
+
 <form method="post" action="/logout">
     <button type="submit">Logout</button>
 </form>
 `
 
 func profilePageHandler(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(response, profilePage)
+	username := getUsername(request)
+	if username != "" {
+		fmt.Fprintf(response, profilePage, username)
+	} else {
+		http.Redirect(response, request, "/", 302)
+	}
 }
 
 func main() {
